4bce6e8e45dc2ea4670289b220cb8d025dad13a9
keval <kevalbhaimiyani5@gmail.com>
2020-07-10 14:45:28 -0400

    IDE_noon practice

--- src/ClassstartingFromOne.java
+++ src/ClassstartingFromOne.java
@@ -1,5 +1,13 @@
 public class ClassstartingFromOne {
+//comment on line
+/*
+    sdhgff
+    sfyjfgh
+            sahjgwhjf
+    sdfggf
+                    sdhejh
 
+    mltpl line comment*/
 
     public static void main(String a[]){
         int num1 = 10;
@@ -8,11 +16,14 @@ public class ClassstartingFromOne {
         float b = 2.3f;
         char  d= 'a';
         char  e= 'A';
+        char  f= '?';
         System.out.println(num1+num2);
         System.out.println(c+b);
         System.out.println((int) c+b);
+        System.out.println((int) f);
         System.out.println((int) d + "\n"+ (int) e);
         System.out.println((char)124);
+        System.out.println("hello world");
 
     }
 }
