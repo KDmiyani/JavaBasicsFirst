546cb361ae2730075cc3ca6fa3daa1f7569db5d2
keval <kevalbhaimiyani5@gmail.com>
2020-07-10 14:32:52 -0400

    Initial commit

---
+++ src/ClassstartingFromOne.java
@@ -0,0 +1,18 @@
+public class ClassstartingFromOne {
+
+
+    public static void main(String a[]){
+        int num1 = 10;
+        int num2 = 2;
+        float c = 2.3f;
+        float b = 2.3f;
+        char  d= 'a';
+        char  e= 'A';
+        System.out.println(num1+num2);
+        System.out.println(c+b);
+        System.out.println((int) c+b);
+        System.out.println((int) d + "\n"+ (int) e);
+        System.out.println((char)124);
+
+    }
+}
